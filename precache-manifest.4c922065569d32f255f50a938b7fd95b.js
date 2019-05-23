63ff46e3186706973ac80e812f177608ec14cf70
bikilone <milosbirovljevic@gmail.com>
2019-05-23 18:02:34 +0200

    Updates

---
+++ precache-manifest.4c922065569d32f255f50a938b7fd95b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a8a938dbe7add5b5800042c9878729",
+    "url": "/tv-shows/index.html"
+  },
+  {
+    "revision": "e4a4ac5734810fd75b56",
+    "url": "/tv-shows/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4f92cfd708f9d099f3c8",
+    "url": "/tv-shows/static/js/2.a3d6a549.chunk.js"
+  },
+  {
+    "revision": "e4a4ac5734810fd75b56",
+    "url": "/tv-shows/static/js/main.d3956766.chunk.js"
+  },
+  {
+    "revision": "7c891d411e28cc3e5240",
+    "url": "/tv-shows/static/js/runtime~main.24168937.js"
+  }
+]);
